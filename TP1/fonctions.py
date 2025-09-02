fe7d5ae5bfde8ab4b3dd71e7d32757d83fbb68c7
Canu Antoine <antoinecanu.pro78@gmail.com>
2025-09-02 03:31:44 -1000

    6.1 Variables

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -2,5 +2,6 @@ def puissance(a, b):
   if type(a) is not int or type(b) is not int:
     raise TypeError("Only intergers are allowed")
 
+  if a == 0 and b < 0:
+    raise ValueError("Value is undefined")
   return a**b
-
