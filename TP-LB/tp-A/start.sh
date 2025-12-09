0c504e64aa683bf3ea5c20f36895a09d6a80a2c6
Canu Antoine <antoinecanu.pro78@gmail.com>
2025-12-09 05:01:01 -1000

    LoadBalancer TP

--- TP-LB/tp-A/start.sh
+++ TP-LB/tp-A/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker network create tp1b > /dev/null 2>&1 
+docker network create tplb > /dev/null 2>&1 
 
-docker build -t im-nginx-lb -f tp-A/Dockerfile .
+docker build -t im-nginx-lb .
 
 mkdir -p shared1 shared2
 
@@ -11,14 +11,23 @@ echo "<h1>Hello 2" > shared2/index.html
 
 docker run -d \
     --name nginx1 \
-    -p 8081:80 \
+    -p 81:80 \
     -v $PWD/shared1:/usr/share/nginx/html \
+    --network tplb \
     --rm \
-    im-nginx-lb 
+    nginx
 
 docker run -d \
     --name nginx2 \
-    -p 8082:80 \
+    -p 82:80 \
     -v $PWD/shared2:/usr/share/nginx/html \
+    --network tplb \
     --rm \
-    im-nginx-lb 
+    nginx
+
+docker run -d \
+    --name loadbalancer \
+    --network tplb \
+    -p 83:80 \
+    --rm \
+    im-nginx-lb
